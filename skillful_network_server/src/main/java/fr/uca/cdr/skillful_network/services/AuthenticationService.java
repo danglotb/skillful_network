b5a15ebcf6e30d73345066c643c057d3609bb1d0
bdanglot <benjamin.danglot@univ-cotedazur.fr>
2020-05-29 09:36:34 +0200

    refactor: move all the business logique from AuthenticationController to a dedicated service

---
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/services/AuthenticationService.java
@@ -0,0 +1,22 @@
+package fr.uca.cdr.skillful_network.services;
+
+import fr.uca.cdr.skillful_network.entities.user.User;
+import org.springframework.security.core.Authentication;
+
+import java.util.Set;
+
+public interface AuthenticationService {
+
+    User getCurrentUser();
+
+    User register(String email, String password, Set<String> roles);
+
+    Authentication authentication(String email, String password);
+
+    String login(User user);
+
+    User whoAmI();
+
+    String resetPassword();
+
+}
