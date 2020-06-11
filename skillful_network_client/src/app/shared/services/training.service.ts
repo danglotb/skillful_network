a214129c7fae36160060c1e17c38c48201b3580d
bdanglot <benjamin.danglot@univ-cotedazur.fr>
2020-06-11 19:05:24 +0200

    refactor: clean up the training service

--- skillful_network_client/src/app/shared/services/training.service.ts
+++ skillful_network_client/src/app/shared/services/training.service.ts
@@ -1,81 +1,29 @@
-import {Injectable} from '@angular/core';
-import {Training} from '../models/application/training';
+import { Injectable } from '@angular/core';
+import { Training } from '../models/application/training';
 import { ApiHelperService } from './api-helper.service';
 import { SearchService } from './abstract-search.service';
 
+const ROOT_ENDPOINT: string = '/trainings/';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TrainingService extends SearchService<Training> {
-    public trainings: Training[];
 
     constructor(protected api: ApiHelperService) {
         super(api);
     }
 
-    public findById(id: number): Training{
-        return this.trainings[id];
+    public getById(id: number): Promise<Training> {
+        return this.api.get({ endpoint: ROOT_ENDPOINT + id });
     }
 
-
-
-// Import depuis le Backend
-    public findAll(): Promise<any> {
-        let promise = new Promise((resolve, reject) => {
-            this.api.get({ endpoint: '/trainings' })
-              .then(
-                res => { 
-                  resolve(res);
-                },
-                msg => { 
-                  reject(msg);
-                  }
-              ).catch((error) => {
-              });
-          });
-          return promise;
-    }
-
-    public findAllByPage(page:number, 
-                        size:number, 
-                        sortOrder:String,
-                        fieldToSort:String): Promise<any> {
-        let promise = new Promise((resolve, reject) => {
-            this.api.get({ endpoint: '/trainings/' ,
-              queryParams: {"page":page, "size":size, "sortOrder":sortOrder, "field":fieldToSort}})
-              .then(
-                res => { 
-                  resolve(res);
-                },
-                msg => { 
-                  reject(msg);
-                  }
-              ).catch((error) => {
-              });
-          });
-          return promise;
+    public getAll(): Promise<Training[]> {
+        return this.api.get({ endpoint: ROOT_ENDPOINT });
     }
 
     public getBySearch(keyword: string, page: number, size: number, sortOrder: string, field: string): Promise<Training> {
         return super._getBySearch('trainings', keyword, page, size, sortOrder, field);
     }
-
-    public getTrainingBySearch(keyword: string, page: number, size: number, order: string, field: string): Promise<any> {
-        let promise = new Promise((resolve, reject) => {
-            this.api.get({ endpoint: `/trainings/search`, queryParams: { keyword: keyword, page: page, size: size, sortOrder: order, field: field } })
-                .then(
-                    res => {
-                        resolve(res);
-                    },
-                    msg => {
-                        reject(msg);
-                    }
-                ).catch((error) => {
-                    console.log("request not found")
-                });
-        });
-        return promise;
-    }
-
+    
 }
