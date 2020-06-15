793b5e6c2128ddf07126b6f1efc0b43d2001f526
danglotb <bdanglot@gmail.com>
2020-06-15 09:21:00 +0200

    ci: specify the pom of the backend

--- .travis/travis-backend-coverage.sh
+++ .travis/travis-backend-coverage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn -Pcoveralls -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID -DdoIntegrationTests=true clean test jacoco:report coveralls:report
+mvn -f skillful_network_server/pom.xml -Pcoveralls -DTRAVIS_JOB_ID=$TRAVIS_JOB_ID clean test jacoco:report coveralls:report -Dspring-boot.run.profiles=test
