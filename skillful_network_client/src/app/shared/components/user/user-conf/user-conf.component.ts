d89bdb5b658a5662f6b5365b5c16d4b4b7f88761
danglotb <bdanglot@gmail.com>
2020-06-16 12:43:53 +0200

    feat: initialize now correctly the date picker

--- skillful_network_client/src/app/shared/components/user/user-conf/user-conf.component.ts
+++ skillful_network_client/src/app/shared/components/user/user-conf/user-conf.component.ts
@@ -1,12 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { DateAdapter } from '@angular/material/core';
 import { User } from 'src/app/shared/models/user/user';
+
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+// Depending on whether rollup is used, moment needs to be imported differently.
+// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
+// syntax. However, rollup creates a synthetic default module and we thus need to import it using
+// the `default as` syntax.
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+// import { default as _rollupMoment } from 'moment';
+
+const moment = _moment;
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-user-conf',
   templateUrl: './user-conf.component.html',
-  styleUrls: ['./user-conf.component.scss']
+  styleUrls: ['./user-conf.component.scss'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 
 export class UserConfComponent {
@@ -27,7 +64,7 @@ export class UserConfComponent {
     this.formGroup = new FormGroup({
       lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
-      birthDate: new FormControl('', Validators.required),
+      birthDate: new FormControl(moment(), Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       mobileNumber: new FormControl('', [Validators.required, Validators.minLength(10)]),
       careerGoal: new FormControl('', [Validators.required, Validators.minLength(3)])
@@ -41,6 +78,7 @@ export class UserConfComponent {
       this.userLogged.mobileNumber = data.mobileNumber;
       this.userLogged.careerGoal = data.careerGoal;
     });
+    this
     if (this.readonly) {
       this.formGroup.disable();
     }
