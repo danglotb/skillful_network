ff401582947ae71123cf0102e8ccd16a560a4de1
Anaëlle <anaelle.alenda@gmail.com>
2020-06-26 19:31:51 +0200

    3.2(aa) feat: fix conflict

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/request/ConfirmationRegisterForm.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/request/ConfirmationRegisterForm.java
@@ -2,24 +2,27 @@ package fr.uca.cdr.skillful_network.request;
 
 import fr.uca.cdr.skillful_network.entities.user.Role;
 
+import javax.validation.GroupSequence;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ConfirmationRegisterForm {
-    @Size(min = 2, max = 20, message = "firstName must be between 2 and 20 characters")
-    private String firstName;
 
-    @Size(min = 2, max = 20, message = "lastName must be between 2 and 20 characters")
+public class ConfirmationRegisterForm {
+    @NotNull
+    @Size(min = 2, max = 20, message = "firstName must be between 2 and 20 characters")
     private String lastName;
+
+    private String firstName;
 
     private Set<String> roleSet;
 
     private ConfirmationRegisterForm(){};
 
-    public ConfirmationRegisterForm(@Size(min = 2, max = 20, message = "firstName must be between 2 and 20 characters") String firstName, @Size(min = 2, max = 20, message = "lastName must be between 2 and 20 characters") String lastName, Set<String> roleSet) {
-        this.firstName = firstName;
+    public ConfirmationRegisterForm(String firstName, @Size(min = 2, max = 20, message = "lastName must be between 2 and 20 characters") String lastName, Set<String> roleSet) {
         this.lastName = lastName;
+        this.firstName = firstName;
         this.roleSet = roleSet;
     }
 
