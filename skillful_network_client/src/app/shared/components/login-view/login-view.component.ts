867939e3a28a38da048d55847f9389320ba3eb6e
Anaëlle <anaelle.alenda@gmail.com>
2020-06-30 17:43:29 +0200

    3.2(aa) review: add content login-view

--- skillful_network_client/src/app/shared/components/login-view/login-view.component.ts
+++ skillful_network_client/src/app/shared/components/login-view/login-view.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'app-login-view',
+  selector: 'app-login-view-component',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.scss']
 })
 export class LoginViewComponent implements OnInit {
 
+  @Input() title:string;
+  @Input() icon:string;
+  @Input() public loginRef: TemplateRef<any>;
+  
   constructor() { }
 
   ngOnInit(): void {
