611b2d89ad4d113a834f7fb970473f9c819cfacb
Anaëlle <anaelle.alenda@gmail.com>
2020-06-30 13:35:50 +0200

    3.2(aa) review: modified files to receive role hashMap<key,value>

--- skillful_network_client/src/app/registration-confirmation/registration-confirmation.component.ts
+++ skillful_network_client/src/app/registration-confirmation/registration-confirmation.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { Role } from '../shared/models/user/role';
 import { AuthService } from '../shared/services/auth.service';
 import { Router } from '@angular/router';
 import { UserService } from '../shared/services/user.service';
+import { RoleService } from '../shared/services/role.service';
+import { async } from '@angular/core/testing';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { newArray } from '@angular/compiler/src/util';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { keyValuesToMap } from '@angular/flex-layout/extended/typings/style/style-transforms';
 
 @Component({
   selector: 'app-registration-confirmation',
@@ -12,47 +17,44 @@ import { UserService } from '../shared/services/user.service';
 })
 export class RegistrationConfirmationComponent implements OnInit {
 
-  //Selection des rôles utilisateurs
-  value: string;
-  viewValue: string;
-  roles: Role[] = [
-    { value: 'ROLE_USER', valueView: 'utilisateur' },
-    { value: 'ROLE_COMPANY', valueView: 'entreprise' },
-    { value: 'ROLE_TRAINING_ORGANIZATION', valueView: 'organisme de formation' }
-  ];
+  roles = new Map();
 
+  //variable pour récupérer la valeur du <mat-selection> du formControl selectRole
   event: Event;
   selectedValue: string = '';
 
   // FormGroup pour le formulaire Registration
   registrationForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private userService: UserService, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private userService: UserService, private roleService: RoleService, private router: Router) {
     this.buildForm();
+    this.getRoles();
   }
 
   ngOnInit(): void {
   }
+  
 
   //intégre les formControl au sein du formGroup
   buildForm() {
     this.registrationForm = this.formBuilder.group({
       selectRole: new FormControl('', Validators.required),
       name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-      firstName: new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(20)])
+      firstName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)])
     });
   }
 
   //Récupère via l'évenement de sélection du select la valeur du role
   getSelected(event: Event): string {
-    this.selectedValue = this.registrationForm.value.selectRole.value
+    this.selectedValue = this.registrationForm.value.selectRole;
+    console.log(this.selectedValue);
     return this.selectedValue;
   }
 
-  //Change le validator selon le rôle
+  //Change le type de validation selon le rôle
   setValidator(event: Event) {
     if (this.getSelected(this.event) == 'ROLE_USER') {
-      this.registrationForm.get('firstName').setValidators([Validators.required,Validators.minLength(3), Validators.maxLength(20)]);
+      this.registrationForm.get('firstName').setValidators([Validators.required, Validators.minLength(3), Validators.maxLength(20)]);
     } else {
       this.registrationForm.get('firstName').setValidators([]);
     }
@@ -61,18 +63,31 @@ export class RegistrationConfirmationComponent implements OnInit {
 
   async registration() {
     let response;
-    if(this.selectedValue == 'ROLE_USER'){
+    if (this.selectedValue == 'ROLE_USER') {
       response = this.userService.updateConfirmationRegister(this.registrationForm.value.firstName, this.registrationForm.value.name, [this.selectedValue]);
-    }else{
+    } else {
       response = this.userService.updateConfirmationRegister(" ", this.registrationForm.value.name, [this.selectedValue]);
     }
     await response.then((data) => {
-        this.authService.user = data;
-        console.log(data);
-        this.router.navigate(['/home']);
-      }).catch((error) => {
-        console.log(error);
-      });
+      this.authService.user = data;
+      console.log(data);
+      this.router.navigate(['/home']);
+    }).catch((error) => {
+      console.log(error);
+    });
   }
+
+  async getRoles() {
+    
+    await this.roleService.getRoles().then((data) => {
+     Object.entries(data).forEach(
+        ([key, value]) => this.roles.set(key,value)
+        );
+     console.log(this.roles);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  
 }
 
