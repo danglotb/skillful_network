f5c61a5fd6d5b1d6e9ab61016732c10e4c1ad76a
YounesTmr <younestemar@gmail.com>
2020-07-03 07:58:14 +0200

    2-yte-apipost-deleteMethod : add the deleting method to the controller post

--- skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/application/PostController.java
+++ skillful_network_server/src/main/java/fr/uca/cdr/skillful_network/controller/application/PostController.java
@@ -2,11 +2,14 @@ package fr.uca.cdr.skillful_network.controller.application;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,4 +35,10 @@ public class PostController {
 	public Post create(@Valid @RequestBody Post post) {
 		return this.postService.createPost(post);
 	}
+	
+	@DeleteMapping(value = "/{id}")
+    @Transactional
+    public void delete(@PathVariable(value = "id") Long id) {
+        postService.deletePostById(id);
+    }
 }
