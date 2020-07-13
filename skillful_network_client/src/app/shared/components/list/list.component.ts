ec87a30b2454c1d42e4b1d662de81a9ca97e77f7
Philou <pfouque@gmail.com>
2020-07-13 19:24:35 +0200

    1.4.2-pf-feat 0.2 prepare list-component for follow column

--- skillful_network_client/src/app/shared/components/list/list.component.ts
+++ skillful_network_client/src/app/shared/components/list/list.component.ts
@@ -11,6 +11,8 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { DomSanitizer } from '@angular/platform-browser';
 import { User } from '../../models/user/user';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-list',
@@ -53,16 +55,20 @@ export class ListComponent implements OnInit {
   }
 
   constructor(
+    private userService: UserService,
+    private authService: AuthService,
     private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.service.getBySearch(this.keyword, this.pageIndex, this.pageSize, this.checkOrder(), this.checkField())
-      .then(res => {
-        this.length = res.totalElements;
-        this.dataSource = new MatTableDataSource<any>(res.content);
-      }).finally(() => this.isLoading = false);
+    this.doSearch(this.keyword);
+    // this.service.getBySearch(this.keyword, this.pageIndex, this.pageSize, this.checkOrder(), this.checkField())
+    //   .then(res => {
+    //     console.log('res.content:');console.log(res.content);
+    //     this.length = res.totalElements;
+    //     this.dataSource = new MatTableDataSource<any>(res.content);
+    //   }).finally(() => this.isLoading = false);
   }
 
   onSearch() {
@@ -71,13 +77,24 @@ export class ListComponent implements OnInit {
       this.pageIndex = this.paginator.pageIndex + 1;
     }
     const keyword = this.search.value.keyword;
-    this.service.getBySearch(keyword, this.pageIndex, this.pageSize, this.checkOrder(), this.checkField())
-      .then((res: { totalElements: number; content: any[]; }) => {
-        this.length = res.totalElements;
-        this.dataSource = new MatTableDataSource<any>(res.content);
-      }).finally(() => this.isLoading = false);
+    this.doSearch(keyword)
+    // this.service.getBySearch(keyword, this.pageIndex, this.pageSize, this.checkOrder(), this.checkField())
+    //   .then((res: { totalElements: number; content: any[]; }) => {
+    //     console.log('res.content:');console.log(res.content);
+    //     this.length = res.totalElements;
+    //     this.dataSource = new MatTableDataSource<any>(res.content);
+    //   }).finally(() => this.isLoading = false);
   }
 
+  doSearch(keyword: string) {
+    this.service.getBySearch(keyword, this.pageIndex, this.pageSize, this.checkOrder(), this.checkField())
+    .then((res: { totalElements: number; content: any[]; }) => {
+      console.log('res.content:');console.log(res.content);
+      this.length = res.totalElements;
+      this.dataSource = new MatTableDataSource<any>(res.content);
+    }).finally(() => this.isLoading = false);
+  }
+  
   checkField() {
     if (this.field == null) {
       return this.field = this.displayedColumns[1];
@@ -95,6 +112,17 @@ export class ListComponent implements OnInit {
     return this.order;
   }
 
+  isStandard(matColumnDef: string) {
+    switch (matColumnDef) {
+      case 'details':
+      case 'picture':
+      case 'followed':
+        return false;
+      default:
+        return true;
+    }
+  }
+
   isDetails(matColumnDef: string) {
     return matColumnDef === 'details';
   }
@@ -102,6 +130,42 @@ export class ListComponent implements OnInit {
   isPicture(matColumnDef: string) {
     return matColumnDef === 'picture';
   }
+
+  isFollowedColumn(matColumnDef: string) {
+    return matColumnDef === 'followed';
+  }
+
+  isFollowed(element: any) : boolean {
+    // console.log('isFollowed() - element:');console.log(element);
+    let currentUser: User  = this.authService.user;
+    let result: boolean = false;
+    element.followableSet.map( (item) => {
+      if (item.id == currentUser.id) {
+        // console.log("FOUND !!!! item.id: "+ item.id); 
+        // console.log('--> currentUser.id: ' + currentUser.id);
+        // console.log('--> element.id: ' + element.id);
+        // console.log('--> element: ');console.log(element);
+        result = true;
+      }
+    } )
+    return result;
+  }
+
+  async follow(element: any) {
+    console.log('follow() - element: ');console.log(element);
+    console.log('element.id: ');console.log(element.id);
+    let currentUser: User  = this.authService.user; 
+    console.log('currentUser.id: ');console.log(currentUser.id);
+    
+  }
+  
+  async unfollow(element: any) {
+    console.log('unfollow() - element:');console.log(element);
+    console.log('element.id: ');console.log(element.id);
+    let currentUser: User  = this.authService.user;
+    console.log('currentUser.id: ');console.log(currentUser.id);
+  }
+
 
   getImage(element: any) {
     if (this.type == 'user') { // working but not respecting the open/close principle, need to be refactored.
